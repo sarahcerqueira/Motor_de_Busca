e85fa81f98106f80b65db511a35a0cbb94e5fefb
sarahecomp <sarahecomp@gmail.com>
2019-10-23 09:51:29 +0100

    Implementação da Comunicação UDP servidor e comunicação via multicast

---
+++ src/serverMulticast/Multicast.java
@@ -0,0 +1,65 @@
+package serverMulticast;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+/**A classe Multicast é responsável pela comunicação multicast entre os servidores.
+ * Nela está configurado de forma estática o ip do grupo multicast como "230.0.0.0" e a porta para 4448.
+ * 
+ *  
+ * */
+public class Multicast {
+	
+	private static String ip_group = "230.0.0.0";
+	private static int port = 4448;
+	private MulticastSocket socket;
+	private InetAddress group;
+	
+	/**Cria o socket multicast e se junta ao grupo. 
+	 * 
+	 * @throws IOException Lança exceção caso o programa não consiga abrir um socket multicast na porta estabelecida.
+	 **/
+	public Multicast() throws IOException {
+		socket = new MulticastSocket(port);
+		group = InetAddress.getByName(ip_group);
+		socket.joinGroup(group);
+	}
+	
+	/**Aceita pacotes UDP do grupo multicast. 
+	 * 
+	 * @param buf 			Vetor de bytes que indica o tamanho do array que vai ser recebido.
+	 * @throws IOException	Lança exceção se não for possível aceitar o pacote recebido.
+	 * @return 				Retorna um vetor de bytes com os dados do pacote recebido.
+	 * */
+	public byte[] acceptPacket(byte[] buf) throws IOException {
+		DatagramPacket packet = new DatagramPacket(buf, buf.length);
+		socket.receive(packet);
+		return packet.getData();
+	}
+	
+	/**Envia pacotes UDP para o grupo multicast. 
+	 * 
+	 * @param buf			Vetor de bytes com os dados que serão enviados.
+	 * @throws IOException	Lança exceção caso não seja possível enviar o pacote.
+	 **/
+	public void sendPacket(byte[] buf) throws IOException {
+		DatagramPacket packet = new DatagramPacket(buf, buf.length, group, port);
+		socket.send(packet);
+	}
+	
+	/**Deixa o grupo e fecha o socket. 
+	 * 
+	 * @throws IOException Lança exceção quando há algum problema ao deixar o grupo, ou fechar a conexão.
+	 * 
+	 */
+	public void closeConection() throws IOException {
+		socket.leaveGroup(group);
+		socket.close();
+	}
+	
+	
+	
+
+}
---
+++ src/util/ServerUDP.java
@@ -0,0 +1,51 @@
+package util;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+/**Representa um servidor UDP, que pode enviar e receber pacotes de clientes.
+ * A porta está configurada para 4446.
+ * 
+ */
+public class ServerUDP {
+	private static int port = 4446;
+	private DatagramSocket severSocket;
+	
+	/**Abre a porta para permitir a troca e pacotes com clientes. 
+	 * 
+	 * @throws SocketException Lança exceção caso não seja possível criar o socket server na porta determinada.
+	 */
+	public ServerUDP() throws SocketException {
+		severSocket = new DatagramSocket(port);
+	}
+	
+	/**Aceita pacotes UDP. 
+	 * 
+	 * @param buf 			Vetor de byte que indica o tamanho do a vetor a ser recebido.
+	 * @throws IOException	Lança exceção caso haja problemas ao receber o pacote.
+	 * @return				Retorna um objeto do tipo PacketReceived, que tem toda as informações do pacote recebido.
+	 */
+	public PacketReceived acceptPacket(byte[] buf) throws IOException{
+		DatagramPacket packet = new DatagramPacket(buf,buf.length);
+		severSocket.receive(packet);
+		return new PacketReceived(packet.getAddress(), packet.getData(), packet.getPort());
+	}
+	
+	/**Envia pacotes UDP. 
+	 * 
+	 * @param ip 			Ip do cliente que receberá o pacote.
+	 * @param port 			Porta onde o cliente receberá o pacote.
+	 * @param buf			Vetor de bytes com os dados que serão enviados ao cliente.
+	 * @throws IOException	Lança exceção caso não seja possível enviar o pacote.
+	 */
+	public void sendPacket(InetAddress ip, int port, byte[] buf) throws IOException {
+		DatagramPacket packet = new DatagramPacket(buf,buf.length, ip, port);
+		severSocket.send(packet);
+	}
+	
+	
+
+}
